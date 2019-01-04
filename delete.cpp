00709ededd7e65135c26e7c65260b8815e0a8da6
Luciferleon <43335100+Luciferleon@users.noreply.github.com>
2019-01-04 16:55:27 +0800

    Create delete.cpp

---
+++ delete.cpp
@@ -0,0 +1,39 @@
+#include"phonebook.h"
+int del( ListNodePtr *sPtr, ListNode item )
+{
+    ListNodePtr previousPtr;
+    ListNodePtr currentPtr;
+    ListNodePtr tempPtr;
+
+
+    if ( item.id == ( *sPtr )->id )
+    {
+        tempPtr = *sPtr;
+        *sPtr = ( *sPtr )->nextPtr;
+        free( tempPtr );
+        return item.id;
+    }
+    else
+    {
+        previousPtr = *sPtr;
+        currentPtr = ( *sPtr )->nextPtr;
+
+
+        while ( currentPtr != NULL && currentPtr->id != item.id )
+        {
+            previousPtr = currentPtr;
+            currentPtr = currentPtr->nextPtr;
+        }
+
+
+        if ( currentPtr != NULL )
+        {
+            tempPtr = currentPtr;
+            previousPtr->nextPtr = currentPtr->nextPtr;
+            free( tempPtr );
+            return item.id;
+        }
+    }
+
+    return '\0';
+}
