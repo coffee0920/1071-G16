4868c4de9e7f11eef5e92e30886bf3b2e5639ec9
miles8877866 <43271915+miles8877866@users.noreply.github.com>
2019-01-04 20:02:39 +0800

    Update insert.cpp

--- insert.cpp
+++ insert.cpp
@@ -4,31 +4,23 @@ void insert( ListNode **sPtr, ListNode item )
     ListNodePtr newPtr;
     ListNodePtr previousPtr;
     ListNodePtr currentPtr;
-
     newPtr = (ListNode *) malloc( sizeof( ListNode ) );
     char *s;
     s = (char *)malloc(sizeof(char)*80);
-
     if ( newPtr != NULL )
     {
         newPtr->id = item.id;
         strcpy(s,item.name);
         newPtr->name = s;
         newPtr->num = item.num;
-
         newPtr->nextPtr = NULL;
-
         previousPtr = NULL;
         currentPtr = *sPtr;
-
-
         while ( currentPtr != NULL && item.id > currentPtr->id )
         {
             previousPtr = currentPtr;
             currentPtr = currentPtr->nextPtr;
         }
-
-
         if ( previousPtr == NULL )
         {
             newPtr->nextPtr = *sPtr;
